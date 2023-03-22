3dccf7becdca6e85e0b62eea9d6f51055406aa3b
matias rodriguez <walo142@hotmail.com>
2023-03-22 11:45:49 -0300

    primercommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './Componentes/encabezado/encabezado.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { SobreMiComponent } from './Componentes/sobre-mi/sobre-mi.component';
+import { SkillComponent } from './Componentes/skill/skill.component';
+import { CurriculumComponent } from './Componentes/curriculum/curriculum.component';
+import { PortfolioComponent } from './Componentes/portfolio/portfolio.component';
+import { ContactoComponent } from './Componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    SobreMiComponent,
+    SkillComponent,
+    CurriculumComponent,
+    PortfolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
